78a3ccc9a13d4cb6bb39be81871ff098f8d0ef47
Deveart <deveartbr@users.noreply.github.com>
2017-01-25 14:58:06 -0300

    Create ShowMore.js

---
+++ Animations/ShowMoreItens/ShowMore.js
@@ -0,0 +1,35 @@
+//adicionar os parametros data-id e data-class em cada btn
+$(function(){
+  // Ativar função de carregar mais galerias
+  $(".carregar_mais").click(function(e){
+    e.preventDefault();
+    var qnt = $(this).data("qnt");
+    if (qnt == undefined ) {
+      qnt = 8;
+    }
+    console.log(qnt + "");
+    var id = $(this).data("id");
+    var classOculta = $(this).data("class");
+    id = "#"+id;
+    var classOcultaAux = classOculta;
+    classOculta = "."+classOculta+":hidden";
+    // Ativar função de carregar mais galerias
+    var pos1 = $(id).offset().top;
+    // Selecionar os proximos 8 itens e exibir eles.
+    $(classOculta).slice(0, qnt).removeClass(classOcultaAux);
+    //posição atual após o deslocamento
+    var pos2 = $(id).offset().top;
+    //Scrolling to anchor.
+    //Regule o tempo na variável variation
+    var variation = 2;
+    var timeScrolling = ($(id).offset().top  - pos1) * variation;
+    $('html, body').delay(150).animate({
+      scrollTop: pos2 -($( window ).height()*0.5)
+    }, timeScrolling);
+    // Caso termine, esconda o botão
+    if($(classOculta).length == 0){
+      $(id).addClass('hide');
+    }
+
+  });
+});
